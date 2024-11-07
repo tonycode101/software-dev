8c9e3589ccc4cf4c4f695d3dc8cf5a4856f39fe5
Anthony <abrahamanthony445@gmail.com>
2024-11-07 15:51:02 +0100

    adding burger btn functions

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 
-var btn = document.getElementById("contactBtn");
-var name = "Anthony";
-btn.addEventListener("click", function () {
-  alert("I'm an hacker dont play with me!" + name);
+
+// this is the burger button
+var burgerBtn = document.getElementById("burger");
+
+// This is the navbar to show and hide
+var nav = document.getElementById("nav");
+
+// when your click the burger button, you add the active class to the navbar
+burgerBtn.addEventListener("click", function () {
+  console.log("You clicked the button")
+
+  // adding or delete the class active
+  nav.classList.toggle("active");
 });
+
